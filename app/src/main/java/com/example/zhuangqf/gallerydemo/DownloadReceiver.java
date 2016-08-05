a917c1c3ea5aaa34b0d528292f63d8563cc00b42
zhuangqf <zhuangqinfa@outlook.com>
2016-08-05 16:56:40 +0800

    beauty code style

--- app/src/main/java/com/example/zhuangqf/gallerydemo/DownloadReceiver.java
+++ app/src/main/java/com/example/zhuangqf/gallerydemo/DownloadReceiver.java
@@ -1,13 +1,10 @@
 package com.example.zhuangqf.gallerydemo;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 public class DownloadReceiver extends BroadcastReceiver {
 
@@ -25,18 +22,19 @@ public class DownloadReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if(intent==null) return;
         final String action = intent.getAction();
-        if(action.equals(DOWNLOAD)){
-            String fileName = intent.getStringExtra(FILE_NAME);
-            Boolean flag = intent.getBooleanExtra(FLAG, true);
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(context)
-                            .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentText(fileName);
-            if(flag) mBuilder.setContentTitle("Download Begin");
-            else mBuilder.setContentTitle("Download Finish");
-            NotificationManager mManager =
-                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            mManager.notify(NOTIFICATION_BEGIN_ID,mBuilder.build());
-         }
+        if(!DOWNLOAD.equals(action)) return;
+
+        String fileName = intent.getStringExtra(FILE_NAME);
+        Boolean flag = intent.getBooleanExtra(FLAG, true);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentText(fileName);
+
+        mBuilder.setContentTitle(flag ? "Download Begin" : "Download Finish");
+
+        NotificationManager mManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mManager.notify(NOTIFICATION_BEGIN_ID,mBuilder.build());
     }
 }
